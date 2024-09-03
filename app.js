c4163338e0b9ebbfec553cef94f2c5043e72b150
Kevin Julian <kevin.julian.372@gmail.com>
2024-09-02 21:26:16 -0500

    actualizacion vista de tablet

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById("contenedor-menu").addEventListener("click", function() {
+    var menu = document.getElementById("menu-desplegable");
+    menu.classList.toggle("show");
+});
