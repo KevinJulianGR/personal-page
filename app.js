d46100bbb4e07b39354ae2dd9b0e55640c65c3b6
Kevin Julian <kevin.julian.372@gmail.com>
2024-09-03 14:57:32 -0500

    actualizacion vista celular

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-document.getElementById("contenedor-menu").addEventListener("click", function() {
+// Abrir y cerrar el menú cuando se hace clic en el contenedor del menú
+document.getElementById("contenedor-menu").addEventListener("click", function(event) {
     var menu = document.getElementById("menu-desplegable");
     menu.classList.toggle("show");
+    event.stopPropagation(); // Evita que el evento se propague al documento
+});
+
+// Cerrar el menú cuando se hace clic fuera del contenedor del menú
+document.addEventListener("click", function(event) {
+    var menu = document.getElementById("menu-desplegable");
+    var contenedorMenu = document.getElementById("contenedor-menu");
+
+    // Verifica si el clic fue fuera del contenedor del menú
+    if (!contenedorMenu.contains(event.target)) {
+        menu.classList.remove("show");
+    }
 });
